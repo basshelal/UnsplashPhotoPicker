e65ffee9dcf0152f96e492f335d8c943a6e49a0c
Bassam Helal <basshelal@gmail.com>
2019-07-21 17:50:59 +0100

    Updated gradle files and gitignore

--- example/src/main/java/com/unsplash/pickerandroid/example/ExampleApplication.kt
+++ example/src/main/java/com/unsplash/pickerandroid/example/ExampleApplication.kt
@@ -1,6 +1,8 @@
 package com.unsplash.pickerandroid.example
 
 import android.app.Application
+import com.unsplash.pickerandroid.example.keys.UNSPLASH_ACCESS_KEY
+import com.unsplash.pickerandroid.example.keys.UNSPLASH_SECRET_KEY
 import com.unsplash.pickerandroid.photopicker.UnsplashPhotoPicker
 
 class ExampleApplication : Application() {
@@ -10,8 +12,8 @@ class ExampleApplication : Application() {
         // initializing the picker library
         UnsplashPhotoPicker.init(
             this,
-            "your access key",
-            "your secret key"
+            UNSPLASH_ACCESS_KEY,
+            UNSPLASH_SECRET_KEY
             /* optional page size (number of photos per page) */
         )
             /* .setLoggingEnabled(true) // if you want to see the http requests */
